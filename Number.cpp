7a09096e35b072e6fb620c6bbc7393bcbf20e3ab
bhavy1204 <bhavysarwa51@gmail.com>
2024-12-21 18:38:39 +0530

    Added difficulty levels

--- Number.cpp
+++ Number.cpp
@@ -4,28 +4,77 @@
 #include <string>
 #include <cstdlib>
 #include <iomanip>
+using namespace std;
 
-class numberGame{
-    int n;
-    public:
+class numberGame
+{
+    int n, guess, hint;
+
+public:
+    int easy()
+    {
+        srand(time(0));
+        n = rand() % 10;
+        cout << n << endl;
+        return n;
+    }
+    int moderate()
+    {
+        srand(time(0));
+        n = rand() % 100;
+        cout << n << endl;
+        return n;
+    }
+    int hard()
+    {
+        srand(time(0));
+        n = rand() % 1000;
+        cout << n << endl;
+        return n;
+    }
+    void getUserNum()
+    {
+        cout << "Enter >> ";
+        cin >> guess;
+    }
+    bool check()
+    {
+        return n == guess ? true : false;
+    }
 };
 
-using namespace std;
 int main()
 {
     system("cls");
-    srand(time(0));
-    int n,guess;
-    n =( rand()%10);
-    cout<<n<<endl;
-    cout<<"Welcome to number Game !  "<<endl;
-    cout<<"Enter your number :- "<<endl;
-    cin>>guess;
-    if (guess==n)
-        cout<<"You won the game !! "<<endl;
-    else{
-        cout<<"Bad luck the number was \""<<n<<"\"\nTry again next time !!."<<endl;
-    }
-    
+    int mode;
+    numberGame n;
+    do
+    {
+        cout << "Welcome to game " << endl;
+        cout << " Enter difficulty " << endl;
+        cout << "1. Easy" << endl;
+        cout << "2. Moderate" << endl;
+        cout << "3. Hard" << endl;
+        cout << "4. exit game" << endl;
+        cin >> mode;
+        switch (mode)
+        {
+        case 1:
+            n.easy();
+            break;
+        case 2:
+            n.moderate();
+            break;
+        case 3:
+            n.hard();
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+        }
+    } while (mode != 4);
+
+    n.getUserNum();
+    cout << n.check();
     return 0;
 }
